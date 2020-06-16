5e45125cb6811c914fbe5d0209b9bf04a6de11c9
Diego Rodrigues <di.rodrigues003@gmail.com>
2020-06-15 22:43:56 -0400

    File I/O implemented

---
+++ README.md
@@ -0,0 +1,82 @@
+# Transcripter
+
+Transcripter is a command line app that takes .wav audio files and converts them into text. Transcripter uses the Speech Recognition python module to perform its operations offline using Sphinx. The audio files processed are split by a time differential given by the user without compromising the original audio. These split files are then converted into text and deleted once the process is done. All is done offline. 
+
+A script is also provided to convert all mp3 files in the directory to .wav files if using a UNIX system.  
+
+## Getting Started
+
+This application is heavily dependent on the Speech Recognition module, therefore reading through their documentation is highly recommended but not completely necessary.  
+
+### Prerequisites
+
+PocketSphinx
+SpeechRecognition Library
+Python 2.6, 2.7, or 3.3+ (required)
+
+
+### Installing
+
+First install PocketSphinx by running the following commands
+
+```
+pip install wheel
+```
+
+followed by
+
+```
+pip install ./third-party/WHEEL_FILENAME
+```
+
+If on OSX or on Linux please follow the amazing guide created by the creators of SpeechRecognition: [Here!](https://github.com/Uberi/speech_recognition/blob/master/reference/pocketsphinx.rst)
+
+Now install SpeechRecognition
+
+```
+pip install SpeechRecognition
+
+```
+Download transcripter.py and mp3_to_wav_converter.sh(optional)
+
+You are all set!
+
+## How to Run
+
+Place transcripter.py in a directory with the .wav file to be converted. 
+The format in which the command should be delivered is stated:
+
+```
+Python transcriter.py NAME_OF_FILE.wav
+```
+
+This will separate the text within a 1 minute time differential. To set a different time difference use the -td flag 
+
+```
+Python transcriter.py NAME_OF_FILE.wav -td [integer value of time in minutes]
+```
+
+The result will be placed in a .txt file named after the original audio. Running the program again with the same file or same file name will simply replace the former .txt file with the new text data. 
+
+If the audio file is formatted as an mp3 and the user is running an UNIX machine, then run the following scrip to convert ALL .mp3 files to .wav
+
+```
+chmod +x mp3_to_wav_converter.sh
+```
+
+Followed by
+
+```
+./script-name-here.sh
+```
+
+## Authors
+
+* **Diego Rodrigues** 
+[diegoro1]( https://github.com/diegoro1)
+
+## Acknowledgments
+
+* Thank you SpeechRecognition team for the awesome library
+
+
--- transcripter.py
+++ transcripter.py
@@ -4,46 +4,69 @@ from os import remove
 from os import path
 import sys
 
-# obtains audio file
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), sys.argv[1])
+# transcribe function that takes audio file and splits it according to the value of td to then process
+# by chuncks
+def transcribe_by_minute(AUDIO_FILE, td, f):
+    for i in range(15):
+        t1 = i * td   
+        t2 = t1 + td
 
-# if -td flag is used then set the time differential as the next cmd arg
-# else use standard 1 minute
-if len(sys.argv) >= 2 and sys.argv[2] == "-td":
-    td = int(sys.argv[3]) * 60000
-else:
-    td = 60000
+        # breaks the audio file for better handling
+        # split points are given by td
+        newAudio = AudioSegment.from_wav(AUDIO_FILE)
+        newAudio = newAudio[t1:t2]
+        audioName = "segment" + str(i) + ".wav"
+        newAudio.export(audioName, format="wav")
 
-print("Processing {} for conversion".format(sys.argv[1]))
-print("Time differential set to {} minutes\n".format(td/60000))
+        #geats audio split for processing
+        audio_split = path.join(path.dirname(path.realpath(__file__)), audioName)
 
-for i in range(15):
-    t1 = i * td   
-    t2 = t1 + td
+        # use the audio file as the audio source
+        r = sr.Recognizer()
+        with sr.AudioFile(audio_split) as source:
+            audio = r.record(source)  # read the entire audio file
 
-    # breaks the audio file for better handling
-    # split points are given by td
-    newAudio = AudioSegment.from_wav(AUDIO_FILE)
-    newAudio = newAudio[t1:t2]
-    audioName = "segment" + str(i) + ".wav"
-    newAudio.export(audioName, format="wav")
+        # recognize speech using Sphinx
+        try:
+            print("Converting...Time frame: {0:0.2f} - {1:0.2f}  minutes.".format(i, i + (td/60000)) + "\n")
+            f.write("Time frame: {0:0.2f} - {1:0.2f}  minutes.\n".format(i, i + (td/60000)) + r.recognize_sphinx(audio) + "\n\n")
+        #except sr.UnknownValueError:
+        #   print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+        
+        remove(audioName)
+        f.flush()
 
-    #geats audio split for processing
-    audio_split = path.join(path.dirname(path.realpath(__file__)), audioName)
+    print("Process complete")
 
-    # use the audio file as the audio source
-    r = sr.Recognizer()
-    with sr.AudioFile(audio_split) as source:
-        audio = r.record(source)  # read the entire audio file
+def main():
+    # obtains audio file
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), sys.argv[1])
 
-    # recognize speech using Sphinx
+    # if -td flag is used then set the time differential as the next cmd arg
+    # else use standard 1 minute
+    if len(sys.argv) >= 2 and sys.argv[2] == "-td":
+        td = int(sys.argv[3]) * 60000
+    else:
+        td = 60000
+
+    print("Processing {} for conversion".format(sys.argv[1]))
+    print("Time differential set to {} minutes\n".format(td/60000))
+
+    # creates .txt file to hold transcript
     try:
-        print("Time frame: {0:0.2f} - {1:0.2f}  minutes.\n".format(i, i + (td/60000)) + r.recognize_sphinx(audio) + "\n")
-    #except sr.UnknownValueError:
-    #   print("Sphinx could not understand audio")
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
-    
-    remove(audioName)
+        file_name = sys.argv[1]
+        file_name.replace('.','')
+        file_name = file_name + ".txt"
+        f = open(file_name, "w+")
 
-print("Process complete")
+    except Exception as e:
+        print("Error {} : ".format(e) ) 
+
+    transcribe_by_minute(AUDIO_FILE,td,f)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
